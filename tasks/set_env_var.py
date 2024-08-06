593abaa0138c69a6e29f0679bc644d1d66934832
db-lyon <david.bingham@nimbasolutions.com>
2024-08-05 19:48:56 -0600

    chore: Update cumulusci.yml to include template option for creating Digital Experience site

--- tasks/set_env_var.py
+++ tasks/set_env_var.py
@@ -6,11 +6,14 @@ class SetEnvVars(BaseTask):
     class Options(CCIOptions):
         name: str = Field(..., description="The name of the experience site")
         url_prefix: str = Field(..., description="The prefix of the experience site")
+        template: str = Field(..., description="The template file to use")
 
     parsed_options: Options
 
     def _run_task(self):
         os.environ['EXPERIENCE_SITE_NAME'] = self.parsed_options.name
         os.environ['EXPERIENCE_SITE_PREFIX'] = self.parsed_options.url_prefix
+        os.environ['EXPERIENCE_SITE_TEMPLATE'] = self.parsed_options.template
         self.logger.info(f"Set EXPERIENCE_SITE_NAME to {self.parsed_options.name}")
         self.logger.info(f"Set EXPERIENCE_SITE_PREFIX to {self.parsed_options.url_prefix}")
+        self.logger.info(f"Set EXPERIENCE_SITE_TEMPLATE to {self.parsed_options.template}")
