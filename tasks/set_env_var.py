4fbe394d03aa8d0ae878c2b8e01b03b71c14a37f
db-lyon <david.bingham@nimbasolutions.com>
2024-08-05 20:11:23 -0600

    chore: Update set_env_var task to set return values

--- tasks/set_env_var.py
+++ tasks/set_env_var.py
@@ -14,6 +14,14 @@ class SetEnvVars(BaseTask):
         os.environ['EXPERIENCE_SITE_NAME'] = self.parsed_options.name
         os.environ['EXPERIENCE_SITE_PREFIX'] = self.parsed_options.url_prefix
         os.environ['EXPERIENCE_SITE_TEMPLATE'] = self.parsed_options.template
+        self._set_return_values()
         self.logger.info(f"Set EXPERIENCE_SITE_NAME to {self.parsed_options.name}")
         self.logger.info(f"Set EXPERIENCE_SITE_PREFIX to {self.parsed_options.url_prefix}")
         self.logger.info(f"Set EXPERIENCE_SITE_TEMPLATE to {self.parsed_options.template}")
+        
+    def _set_return_values(self):
+        self.return_values = {
+            "name": self.parsed_options.name,
+            "url_prefix": self.parsed_options.url_prefix,
+            "template": self.parsed_options.template,
+        }
