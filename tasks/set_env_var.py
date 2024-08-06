ac31811b512263da059581132407f9337b502c0b
db-lyon <david.bingham@nimbasolutions.com>
2024-08-05 20:46:41 -0600

    chore: Update cumulusci.yml to include template option for creating Digital Experience site

--- tasks/set_env_var.py
+++ tasks/set_env_var.py
@@ -5,6 +5,7 @@ from cumulusci.utils.options import Field
 class SetEnvVars(BaseTask):
     class Options(CCIOptions):
         name: str = Field(..., description="The name of the experience site")
+        path: str = Field(..., description="The path of the experience bundle and related metadata")
         url_prefix: str = Field(..., description="The prefix of the experience site")
         template: str = Field(..., description="The template file to use")
 
@@ -12,10 +13,12 @@ class SetEnvVars(BaseTask):
 
     def _run_task(self):
         os.environ['EXPERIENCE_SITE_NAME'] = self.parsed_options.name
+        os.environ['EXPERIENCE_SITE_PATH'] = self.parsed_options.path
         os.environ['EXPERIENCE_SITE_PREFIX'] = self.parsed_options.url_prefix
         os.environ['EXPERIENCE_SITE_TEMPLATE'] = self.parsed_options.template
         self._set_return_values()
         self.logger.info(f"Set EXPERIENCE_SITE_NAME to {self.parsed_options.name}")
+        self.logger.info(f"Set EXPERIENCE_SITE_PATH to {self.parsed_options.path}")
         self.logger.info(f"Set EXPERIENCE_SITE_PREFIX to {self.parsed_options.url_prefix}")
         self.logger.info(f"Set EXPERIENCE_SITE_TEMPLATE to {self.parsed_options.template}")
         
@@ -24,4 +27,5 @@ class SetEnvVars(BaseTask):
             "EXPERIENCE_SITE_NAME": self.parsed_options.name,
             "EXPERIENCE_SITE_PREFIX": self.parsed_options.url_prefix,
             "EXPERIENCE_SITE_TEMPLATE": self.parsed_options.template,
+            "EXPERIENCE_SITE_PATH": self.parsed_options.path,
         }
